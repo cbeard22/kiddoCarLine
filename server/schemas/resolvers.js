41c3627115ac6cb6c16946fdc8c0343ba9a9e5ef
Chris Beard <cbeard@hnoj.org>
2021-09-24 18:08:45 -0500

    fixing folders and adding student car cards

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,16 @@
 const { User } = require("../models");
-const { signToken } =require("../utils/auth");
+const { signToken } = require("../utils/auth");
+
 
 const resolvers = {
     Query: {
         activeUser: async () => {
-            
-                const userData = await User.find({});
-                return userData
-        
+            const userData = await User.find({});
+            return userData
         }
     },
-
     Mutation: {
-        createUser: async (parent, args) => {
+        createUser: async (user, args) => {
             const newUser = await User.create(args);
             const token = signToken(newUser)
 
@@ -21,4 +19,4 @@ const resolvers = {
     }
 }
 
-module.exports = resolvers
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,28 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        isParent: Boolean
+        isAdmin: Boolean
+        parentTwo: String
+    }
+
+    type Student {
+        _id: ID
+        student: String
+        parentID: ID
+    }
+
+    type Query {
+        user: [User]
+        students(_id: String): [Student]
+    }
+
+    type Mutation {
+        createLocation(student: String!, tech2: String!): Location
+        createStudent(_id: String!, techNum: Int!): Student
+      }
 `
+
+module.exports = typeDefs;

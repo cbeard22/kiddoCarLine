4824617a2cdc7f19e4e2cc88e8419205c8d72e8d
Jeff Severson <78665465+jeffseverson2001@users.noreply.github.com>
2021-09-28 19:23:14 -0500

    Update seed.js
    Forgot to remove Student and Location Links

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,8 +1,6 @@
 const db = require('../config/connection');
-const { Location, User, Student } = require('../models');
+const { User } = require('../models');
 const UserSeeds = require('./userSeeds.json');
-const LocationSeeds = require('./locationSeeds.json');
-const studentSeeds = require('./studentSeeds.json');
 
 db.once('open', async () => {
   try {

2981e7061efe7811770cbd00900e95bd58f5c4f3
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-30 20:12:51 -0500

    homepage update, adding logo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,29 +2,37 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import img from './images/logo.png';
 
 class Home extends Component {
   render() {
     return (
       <section className='background'>
-      <div style={{ width: '100', margin: '20px'}}>
-        <Grid className="home-grid">
-          <Cell col={6}>
+      <Container style={{ width: '100', margin: '20px'}}>
+        <Row>
+          <Col>
+            <Grid className="home-grid">
+              <Cell col={6}>
 
-            <div className="banner-text">
-              <h1 className='comp-name'> Kiddo Car Line </h1>
-            </div>
-          </Cell>
-          <Cell col={6}>
-            <h3 className='what'> What would you like to do?</h3>
-            <ul className="list">
-              <li><Link to='/SignupForm'> Create a parent account</Link></li>
-              <li><Link to='/ParentLoginForm'> Login as a parent</Link></li>
-              <li><Link to='/AdminLoginForm'> Login as an Administrator</Link></li>
-            </ul>
-          </Cell >
-        </Grid >
-      </div >
+                <div className="banner-text">
+                  <img class='logo' src={img} alt='logo'>
+
+                  </img>
+                </div>
+              </Cell>
+              <Cell col={6}>
+                <h3 className='what m-3'> What would you like to do?</h3>
+                <ListGroup className="list">
+                  <ListGroupItem><Link to='/SignupForm'> Create an Account</Link></ListGroupItem>
+                  <ListGroupItem><Link to='/ParentLoginForm'> Login</Link></ListGroupItem>
+                  <ListGroupItem><Link to='/AdminLoginForm'> Admin Login </Link></ListGroupItem>
+                </ListGroup>
+              </Cell >
+            </Grid >
+          </Col>
+        </Row>
+      </Container >
       </section>
     )
   }

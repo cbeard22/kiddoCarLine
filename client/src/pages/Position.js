d2353f29cc6fb1cdc433f1523923ff65f8d15de9
jeffseverson2001 <jeff@jefflizseverson.com>
2021-10-02 08:42:24 -0500

    Added Row and Position Back

--- client/src/pages/Position.js
+++ client/src/pages/Position.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useMutation } from '@apollo/client';
 import { CREATE_LOCATION } from '../utils/mutations';
 
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+
 const userID = localStorage.getItem('id_user');
 
 const Position = (props) => {
@@ -22,10 +24,26 @@ const Position = (props) => {
 
     return (
         <div className='location'>
-            <h1>I am here for my child!</h1>
+            <h1>Where are you located in the car rider line?</h1>
+                <div className='location-row'>
+                    <DropdownButton id="dropdown-basic-button" title="What Row are you in?">
+                        <Dropdown.Item href="">Row 1</Dropdown.Item>
+                        <Dropdown.Item href="">Row 2</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className='location-position'>
+                    <DropdownButton id="dropdown-basic-button" title="What position are you in?">
+                        <Dropdown.Item href="">Car 1</Dropdown.Item>
+                        <Dropdown.Item href="">Car 2</Dropdown.Item>
+                        <Dropdown.Item href="">Car 3</Dropdown.Item>
+                        <Dropdown.Item href="">Car 4</Dropdown.Item>
+                        <Dropdown.Item href="#">Car 5</Dropdown.Item>
+                        <Dropdown.Item href="#">Car 6</Dropdown.Item>
+                    </DropdownButton>
+                </div>
                 <button className="btn btn-block btn-primary" style={{ cursor: 'pointer' }} onClick={locationSubmit}>
                 I am here for my child!
-              </button>
+                </button>
         </div>
     )
 }
